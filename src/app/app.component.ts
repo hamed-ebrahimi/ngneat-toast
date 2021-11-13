6439aa28fb27243636d1d5a53e0529f407aec66e
Hamed Ebrahimi <ham.3br@gmail.com>
2021-11-13 13:29:45 +0330

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { Component, ViewChild } from '@angular/core';
+import { HotToastService } from '@ngneat/hot-toast';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngneat-toast';
+
+  constructor(private toastService: HotToastService){}
+  
+  normalToast() {
+    this.toastService.show('Normal toast!')
+  }
+  infoToast() {
+    this.toastService.info('Toasts could be ridiculously long and still look fine!')
+  }
+  loadingToast() {
+    this.toastService.loading('Please wait...')
+  }
+  customToast() {
+      this.toastService.show('toasts are customizable!',{
+        dismissible:true,
+        position:'bottom-left',
+        icon: '✌🏼'
+      })
+    }
+  darkToast() {
+    this.toastService.show('Snackbar',{theme:'snackbar',position:'bottom-center'})
+  }
+  markupToast() {
+    this.toastService.show(`you can put <b>HTML Markup</b> in a toast`)
+  }
 }
